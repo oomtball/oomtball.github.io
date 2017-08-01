18f721654f9c042724ee7cd24de8822f8588007f
unknown <welly061540@gmail.com>
2017-08-01 13:22:01 +0800

    Update success

--- brackets/html/gg.js
+++ brackets/html/gg.js
@@ -1,37 +1,28 @@
-let count = 0
-let btnClick = document.getElementById('btn-click')
-let count_display = document.getElementById('count_display')
-
-btnClick.addEventListener('click', (event) => {
-    count++
-    count_display.innerHTML = count
-})
-
-let items = [
-    ['Cursor', 15, 0.1, 0, 0],
-    ['Grandma', 100, 0.5, 0, 0],
-    ['Farm', 500, 4, 0, 0],
-    ['Factory', 3000, 10, 0, 0],
-    ['Machine', 10000, 40, 0, 0],
-    ['Shipment', 40000, 100, 0, 0],
-    ['Alchemy Lab', 200000, 400, 0, 0],
-    ['Portal', 1666666, 6666, 0, 0],
-    ['Time Machine', 123456789, 98765, 0, 0],
-]
-
-let itemList = document.getElementById('item-list')
-
-for(let i in items) {
-    let item = document.createElement('li')
-    let button = document.createElement('button')
-    button.innerHTML = items[i][1]
-    button.setAttribute('data-add', items[i][1])
-    button.addEventListener('click', (event) => {
-        let number = event.target.getAttribute('data-add')
-        count += parseInt(number)
-        count_display.innerHTML = count
-    })
-    item.appendChild(button)
-    itemList.appendChild(item)
+$('#goButton').click(function(){
+    $('html,body').animate({scrollTop:$('#menu').offset().top},800);
+});
+$('.tag1').click(function(){
+    $('html,body').animate({scrollTop:$('.intro1').offset().top},800);
+});
+ $('.tag2').click(function(){
+    $('html,body').animate({scrollTop:$('#experience').offset().top},800);
+});
+ $('.tag3').click(function(){
+    $('html,body').animate({scrollTop:$('#contact').offset().top},800);
+});
+function changePic()
+{
+    document.getElementById('pic1').src = "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/13781871_1048455328525666_8370355559479960474_n.jpg?oh=53f4e5e4c59cac59775e352cdfe8229f&oe=5A03FB78"
 }
-
+function changePicBack()
+{
+    document.getElementById('pic1').src = "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/16195802_1213495102021687_1668290208568003440_n.jpg?oh=2e6d940c3226056b6164d95203fcabae&oe=59F660D9"
+}
+function changePic2()
+{
+    document.getElementById('pic2').src = "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/19665578_1374474032590459_6883774096802214640_n.jpg?oh=03680cf2eed0b47b4d278b76436b6c21&oe=59FD2B69"
+}
+function changePicBack2()
+{
+    document.getElementById('pic2').src = "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/14332956_1088203374550861_7434399787886402661_n.jpg?oh=af67cc4f5dc5c581f5fcabb200f1c652&oe=5A30F902"
+}
